830377835c30c1a9f4f57e3cc7f325f6dc677193
savindi-weerakoon <savindidweerakoon@gmail.com>
2025-04-18 23:13:54 +0530

    updates

--- app/app/auth-wrapper.js
+++ app/app/auth-wrapper.js
@@ -5,10 +5,11 @@ import { AuthProvider } from 'react-oidc-context';
 const oidcConfig = {
   authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY,
   client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
-  redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI,
+  redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URL,
   post_logout_redirect_uri: process.env.NEXT_PUBLIC_COGNITO_LOGOUT_URI,
   response_type: process.env.NEXT_PUBLIC_COGNITO_RESPONSE_TYPE,
   scope: process.env.NEXT_PUBLIC_COGNITO_SCOPE,
+  domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
 };
 
 export default function AuthProviderWrapper({ children }) {
