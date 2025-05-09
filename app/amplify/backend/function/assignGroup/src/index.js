e6a475a15b5e91a93cab194666840af8555b571b
savindi-weerakoon <savindidweerakoon@gmail.com>
2025-05-09 22:47:36 +0530

    deletion of old amplify account

--- app/amplify/backend/function/assignGroup/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
-
-const app = express();
-app.use(bodyParser.json());
-app.use('/', routes);
-
-module.exports = app;
